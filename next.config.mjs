7423f8d0e064982494a2abeed98f6b52cfe304b0
Mohit Shakya <mohit@Mohits-MacBook-Air.local>
2025-12-08 21:23:08 +0530

    chore: use JS next config

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["..."],
+  },
+  // copy all your other options here
+};
+
+export default nextConfig;
