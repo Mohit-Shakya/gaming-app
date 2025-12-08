d51df4472a486c33d3fb313d855bac6856e75e68
Mohit Shakya <mohit@Mohits-MacBook-Air.local>
2025-12-08 21:27:14 +0530

    fix: replace Geist font with Inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-import { Geist, Geist_Mono, Orbitron, Rajdhani } from "next/font/google";
+import { Inter, Roboto_Mono, Orbitron, Rajdhani } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const geistSans = Inter({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = Roboto_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
